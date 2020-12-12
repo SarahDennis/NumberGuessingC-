27a26c0fcf189389c711b9b13cbb098bb9e4658b
Sarah Dennis <sarah.dennis@mymail.champlain.edu>
2020-12-12 10:42:02 -0500

    Adding base files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Documents
+{
+    class Program
+    {
+
+        //Main Function
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
