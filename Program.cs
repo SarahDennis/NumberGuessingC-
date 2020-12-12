37a9d1e053af43cc9cf610b91caffe903a008690
Sarah Dennis <sarah.dennis@mymail.champlain.edu>
2020-12-12 18:47:20 -0500

    Finished

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace Documents
         {
             Numbers n = new Numbers();
 
+
             n.MainInterface();
+            n.game(n.NumtoGuess());
 
 
         }
@@ -29,12 +31,19 @@ namespace Documents
         }
 
 
-        private void WrongAns(){
-            Console.WriteLine("You guessed incorrectly, try again");
+        private void WrongAnsHigher(){
+            Console.WriteLine("You guessed incorrectly. \n" +
+                    "The correct number is higher, try again.");
         }
 
-        private void CorrectAns(){
-            Console.WriteLine("You guessed correct! Good job");
+        private void WrongAnsLower(){
+            Console.WriteLine("You guessed incorrectly. \n" +
+                    "The correct number is lower, try again.");
+        }
+
+        private void CorrectAns(int count){
+            Console.WriteLine("You guessed correct! It took " + count +
+                    " tries. \nGood job!");
         }
 
 
@@ -46,9 +55,51 @@ namespace Documents
         private int NumtoGuess(){
             Random random = new Random();
 
-            return random.Next(188);
+            return random.Next(100);
         }
 
+
+        //The game putting the functions together
+        private void game(int correctNum){   
+            Numbers n = new Numbers();
+            bool correct = false;
+            int count = 1;
+
+
+            //Makes a loop for the game
+            while (correct == false){
+                Console.WriteLine("Guess " + count);
+                string guess = Console.ReadLine();
+
+
+                //If the user prompts to exit the program will stop
+                if (guess == "exit"){
+                    Console.WriteLine("Thank you for playing");
+                    System.Environment.Exit(1);
+
+                } 
+
+
+                //Makes guess into an int and checks to see where the guess lies
+                try{
+                        if (Int16.Parse(guess) == correctNum){
+                            n.CorrectAns(count);
+                            break;
+                        } else if (Int16.Parse(guess) > correctNum) {
+                            n.WrongAnsLower();
+                        } else if (Int16.Parse(guess) < correctNum){
+                            n.WrongAnsHigher();
+                        }
+                        
+                }catch{
+                        Console.WriteLine("That is not a valid answer, Try again");
+                    }
+                
+                count++; 
+
+            }
+
+        }
 
 
 
