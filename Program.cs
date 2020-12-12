b9841c75615eb4e7c42332283fac6f93934f8407
Sarah Dennis <sarah.dennis@mymail.champlain.edu>
2020-12-12 11:05:41 -0500

    Added interface functions and generate number function

--- Program.cs
+++ Program.cs
@@ -2,13 +2,59 @@
 
 namespace Documents
 {
-    class Program
+    class Numbers
     {
 
         //Main Function
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Numbers n = new Numbers();
+
+            n.MainInterface();
+
+
         }
+
+
+
+        //******************************************************************
+        //System Prompts
+        //******************************************************************
+
+        //This is the main interface where the user gets instructions
+        private void MainInterface(){
+            Console.WriteLine("Number Guessing Game \n\n" +
+                "Guess the number 1-100 right in the least amount of tries to win!\n" +
+                "If you ever want to quit type 'exit'");
+        }
+
+
+        private void WrongAns(){
+            Console.WriteLine("You guessed incorrectly, try again");
+        }
+
+        private void CorrectAns(){
+            Console.WriteLine("You guessed correct! Good job");
+        }
+
+
+        //******************************************************************
+        //Technical Functions
+        //******************************************************************
+
+        //Creates the random number to be guessed
+        private int NumtoGuess(){
+            Random random = new Random();
+
+            return random.Next(188);
+        }
+
+
+
+
+
+
+
+
     }
 }
